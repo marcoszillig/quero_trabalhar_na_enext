1067f8853f4618727a053d62f14e9272c5d365ce
Marcos Zillig <marcos.zillig@gmail.com>
2018-08-14 11:08:42 -0300

    create element for modal

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
 $(function () {
-	const base_url = '../potions.json';
 
+	const base_url = '../potions.json';
+	let potion = []
 	$.getJSON(base_url, (data) => {
 		$.each(data.potions, (index, value) => {
 			$('.products')
 				.append(`
-					<li onclick="openModal">
+					<li class="item" id="${value.id}">
 						<img src="${value.image}" alt="${value.name}" class="img-responsive">
 						<h4>${value.name} -</h4>
 						<span> $${value.price}
 					</li>`)
 		})
+		const potions = Object.values(data.potions)
+		potions.forEach( el => {
+			potion.push(el)
+		});
+		console.log(potion[0].id)
+
+		$('.item').on('click', function(){
+			let item = $(this).attr('id')
+			if((item - 1) != potion[item].id){
+				console.log(potion[item].id)			
+				console.log(item)
+			}
+			
+		})
 	})
 })
-function openModal(){
-	document.getElementById('main_content').innerHTML('Teste');
-}
